d65071ea2badfa14406ba04aeb49c77dbc488296
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:35:50 +1200

    Update max.py

--- max.py
+++ max.py
@@ -1,4 +1,7 @@
-a= input("get a number:")
+# a simple program to understand the concept of max function
+
+a= input("get a number:")          # using inbuilt input function
 b= input("get another number:")
+Comparing the value of variables a, b and displaying the larger value
 result= max(a,b)
 print("The larger number is:", result)
