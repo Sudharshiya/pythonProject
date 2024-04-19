c1960d25114efa825729561f964296d9968ffb23
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:06:25 +1200

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,5 +1,8 @@
+# a dictionary can be created by placing a sequence of elements within curly {} braces, separated by a ‘comma’.
+
+#a simple program to display the key with the value
 dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}
 
 for key in dictionary.keys():
-    print(key, "->", dictionary[key])
+    print(key, "->", dictionary[key])   #print the dictionary content with an added symbol -> instead of :
 
