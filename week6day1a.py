630cf0df3302010e14de4c71306a886a3b6d95a7
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:48:41 +1200

    Update week6day1a.py

--- week6day1a.py
+++ week6day1a.py
@@ -1,4 +1,7 @@
+#class with functions and methods
+
 class Fruit:
+    #initialise the parameters used in the class
     def __init__(self, name, color):
         self.name=name
         self.color=color
@@ -7,4 +10,4 @@ class Fruit:
         print("Fruit is "+ self.name)
 
 f1= Fruit("Apple", "Red")
-print(f1.func())
+print(f1.func())       #invocate the function
