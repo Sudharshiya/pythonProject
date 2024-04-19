f4fe4eec0a42eef81187a5ee42a3ac0b75c524af
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:37:38 +1200

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,10 +1,12 @@
+# basic operators used for calculation
+
 a = 9
 b = 4
-add = a + b
-sub = a - b
-mul = a * b
-mod = a % b
-print(add)
+add = a + b  #adding two values
+sub = a - b   #subtracting two values
+mul = a * b    # multiplying two values
+mod = a % b     #finding the remainder of divifing two values
+print(add)     #display the output using print function
 print(sub)
 print(mul)
 print(mod)
