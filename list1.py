ef7c940b899463f42392fc6dc13df3312aba767c
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:22:27 +1200

    Update list1.py

--- list1.py
+++ list1.py
@@ -1,5 +1,12 @@
+# Assigning the value for the list of array
+
 lst = [1, 2, 3, 4, 5]
+
+#inserting a value in the existing list
 lst.insert(2, 6)
+
+#deleting a value in the existing the list with the index number
 del lst[0]
 
+#print the new updated list
 print(lst)
