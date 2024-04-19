8b89f76a984171210e65fdcfb05dc64afbdc8a58
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:45:13 +1200

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,5 +1,8 @@
+#listing the values for the array my_list
+
 my_list = [10, 1, 8, 3, 5]
 
+#swaping the values of the indexes
 my_list[0], my_list[4] = my_list[4], my_list[0]
 my_list[1], my_list[3] = my_list[3], my_list[1]
 
