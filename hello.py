30fe652c30bf08ea84ba187ed2fc9aa1576a3fb8
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:13:48 +1200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
+#Basic Hello everyone program of joining two strings
+
 word1 = "hello"
 word2 = "everyone"
-greet= word1 + word2
-print("Hi", greet)
+greet= word1 + word2  #string concatenation
+print("Hi", greet)    #display greet, the output of the string concatenation
