43fb9ab3e2321052eb5a660f6eda4953373299c8
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:32:13 +1200

    Update logicalop.py

--- logicalop.py
+++ logicalop.py
@@ -1,11 +1,15 @@
+#A simple program to learn the logical operators like and, or, not...etc
+
+#assigning the values to x and y variable
 x = 4
 y = 1
 
-a = x & y
-b = x | y
-c = ~x
-d = x ^ 5
-e = x >> 2
-f = x << 2
+# assigning the output of the logical operations with a variable
+a = x & y    #and operator
+b = x | y    #or operator
+c = ~x       # not operator
+d = x ^ 5    # bitwise xor
+e = x >> 2    # bitwise right shift operator
+f = x << 2     #bitwise left shift operator
 
 print(a, b, c, d, e, f)
