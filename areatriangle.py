acc64c192c784c72510dd86f0b2b29a273ebe6cc
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 12:55:10 +1200

    Update areatriangle.py

--- areatriangle.py
+++ areatriangle.py
@@ -1,6 +1,13 @@
+#a simple program to calculate area of the triangle
+
+# Assigning a value to the variables a,b,c
 a = 7
 b = 8
 c = 9
+
+#calculate the value of s in the Area formula
 s = (a+b+c)/2
+
+#Calculate the area of the triangle
 Area=(s*(s-a)*(s-b)*(s-c))**0.5
-print(Area)
+print(Area) # Display the result of the area
