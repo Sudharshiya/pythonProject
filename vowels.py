3547b437cb861744b31452408b72f0c28a2ad80c
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 13:47:30 +1200

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,6 +1,8 @@
+#Check for the vowels in the message
+
 message=str(input("Enter your message:"))
 count=0
 for letters in message:
-    if letters == "a" or letters == "e" or letters == "i" or letters == "o" or letters == "u":
-        count += 1
-print("The number of vowels in message:", count)
+    if letters == "a" or letters == "e" or letters == "i" or letters == "o" or letters == "u":        #vowels in lowercase will be found
+        count += 1          #count the vowels one by one in the message and add it together
+print("The number of vowels in message:", count)      #display the count of vowels in the message
