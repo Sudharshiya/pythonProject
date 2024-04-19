ae984d9e9f457a99d4d3d665fe3f9f582c541549
Sudharshiya <151153540+Sudharshiya@users.noreply.github.com>
2024-04-19 12:52:24 +1200

    Update area.py

--- area.py
+++ area.py
@@ -1,6 +1,9 @@
+#Simple program to calculate the area of the rectangle
+
+
 #width of the rectangle is w
 w=5
 #length of the rectangle is l
 l=3
-a= w*l
-print ("area of the rectangle=" , a)
+a= w*l          #Area of the rectangle formula
+print ("area of the rectangle=" , a)    # display the output
